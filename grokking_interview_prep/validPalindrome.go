e820f50dee1cac60f92801fea35c83a47b83225c
Bisshwajit Samanta <bisshwajitsamanta@gmail.com>
2025-11-19 17:03:52 +0530

    Grokking Golang

---
+++ grokking_interview_prep/validPalindrome.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"unicode"
+)
+
+func main() {
+	inputString := "RACECAR"
+	fmt.Printf("%s ::=> is a Valid Palindrome %v\n", inputString, ValidPalindrome(inputString))
+}
+
+func ValidPalindrome(inputString string) bool {
+
+	left, right := 0, len(inputString)-1
+
+	for left < right && !unicode.IsLetter(rune(inputString[left])) && !unicode.IsDigit(rune(inputString[left])) {
+		left++
+	}
+	for left < right && !unicode.IsLetter(rune(inputString[right])) && !unicode.IsDigit(rune(inputString[right])) {
+		right--
+	}
+	for unicode.ToLower(rune(inputString[left])) != unicode.ToLower(rune(inputString[right])) {
+		return false
+	}
+	left++
+	right--
+
+	return true
+}
